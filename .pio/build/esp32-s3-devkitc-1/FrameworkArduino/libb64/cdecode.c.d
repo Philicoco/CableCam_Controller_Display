1cfaf8fc9feecdf074a213bf84870ecbe41bd762
Philios <philios.tomo@gmail.com>
2023-10-20 00:04:51 +0200

    Test

--- .pio/build/esp32-s3-devkitc-1/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/FrameworkArduino/libb64/cdecode.c.o: \
- C:/Users/Philios/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- C:/Users/Philios/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
--- .pio/build/esp32-s3-devkitc-1/src/Touch/TouchLibInterface.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/src/Touch/TouchLibInterface.cpp.o: \
- src/Touch/TouchLibInterface.cpp src/Touch/TouchLibInterface.hpp
